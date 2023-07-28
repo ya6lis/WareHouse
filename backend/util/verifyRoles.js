998d047418576c55073bd1c7acfecf132fbfbc6d
Yaroslav Petryshyn <ya6liszl@gmail.com>
2023-07-28 15:40:47 +0300

    Small changes

--- backend/util/verifyRoles.js
+++ backend/util/verifyRoles.js
@@ -5,7 +5,7 @@ const {
 const verifyRoles = (allowedRole) => {
     return async (req, res, next) => {
         const data = await getUserDataForAuth(req.user)
-        if(!data.is_admin === allowedRole){
+        if(!(data.is_admin === allowedRole)){
             return res.sendStatus(403);
         }
         next();
